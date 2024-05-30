dd2bd14b20f15102ded81f390c441a23c275285c
Azizbek <begmatovaziz444@gmail.com>
2024-05-30 22:49:16 +0500

    ready

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 
 const Todos = () => {
   const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([
-    { id: 1, text: "Playing cards", completed: false },
-  ]);
+  const [todos, setTodos] = useState([]);
+  const [incompleteTodos, setIncompleteTodos] = useState(0);
+  const [filter, setFilter] = useState("all");
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -14,13 +14,46 @@ const Todos = () => {
         { id: todos.length + 1, text: todo, completed: false },
       ]);
       setTodo("");
+      setIncompleteTodos(incompleteTodos + 1);
     }
   }
 
   function toggleTodo(id) {
     setTodos(
-      todos.map((t) => (t.id === id ? { ...t, completed: !t.completed } : t))
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
     );
+    setIncompleteTodos(
+      todos.find((todo) => todo.id === id && !todo.completed)
+        ? incompleteTodos - 1
+        : incompleteTodos + 1
+    );
+  }
+
+  function deleteTodo(id) {
+    const deletedTodo = todos.find((todo) => todo.id === id);
+    setTodos(todos.filter((todo) => todo.id !== id));
+    setIncompleteTodos(
+      deletedTodo.completed ? incompleteTodos : incompleteTodos - 1
+    );
+  }
+
+  function clearAllCompleted() {
+    setTodos(todos.filter((todo) => !todo.completed));
+    setIncompleteTodos(todos.filter((todo) => !todo.completed).length);
+  }
+
+  function allTodos() {
+    setFilter("all");
+  }
+
+  function activeTodos() {
+    setFilter("active");
+  }
+
+  function completedTodos() {
+    setFilter("completed");
   }
 
   return (
@@ -38,26 +71,43 @@ const Todos = () => {
           />
         </form>
         <ul>
-          {todos.map((item) => (
-            <li key={item.id}>
-              <div className="todo-wrapper">
-                <input
-                  type="checkbox"
-                  checked={item.completed}
-                  onChange={() => toggleTodo(item.id)}
-                />
-                <p
-                  style={{
-                    textDecoration: item.completed ? "line-through" : "none",
-                  }}
-                >
-                  {item.text}
-                </p>
-              </div>
-              <button className="delete">X</button>
-            </li>
-          ))}
+          {todos
+            .filter((todo) => {
+              if (filter === "all") return true;
+              if (filter === "active") return !todo.completed;
+              if (filter === "completed") return todo.completed;
+            })
+            .map((item) => (
+              <li key={item.id}>
+                <div className="todo-wrapper">
+                  <input
+                    type="checkbox"
+                    checked={item.completed}
+                    onChange={() => toggleTodo(item.id)}
+                  />
+                  <p
+                    style={{
+                      textDecoration: item.completed ? "line-through" : "none",
+                    }}
+                  >
+                    {item.text}
+                  </p>
+                </div>
+                <button onClick={() => deleteTodo(item.id)} className="delete">
+                  X
+                </button>
+              </li>
+            ))}
         </ul>
+        <footer className="">
+          <span>{incompleteTodos} items left</span>
+          <div className="footer-center">
+            <span onClick={allTodos}>All</span>
+            <span onClick={activeTodos}>Active</span>
+            <span onClick={completedTodos}>Completed</span>
+          </div>
+          <span onClick={clearAllCompleted}>Clear Completed</span>
+        </footer>
       </div>
     </header>
   );
